8f53dd5b059c608b7b0fc7b955d9879916458179
JLazar0 <jlazaro1988@gmail.com>
2026-05-09 23:58:01 +0200

    añadimos actuator

---
+++ test-chatbot-render.ps1
@@ -0,0 +1,48 @@
+[Console]::InputEncoding  = [System.Text.UTF8Encoding]::new()
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+
+$guid = [guid]::NewGuid()
+
+Write-Host "GUID SESSION:"  $guid
+
+while ($true) {
+
+    $prompt = Read-Host "`nYOU"
+
+    if ($prompt -eq "exit") {
+        break
+    }
+
+    $body = @{
+        sessionId = $guid
+        message   = $prompt
+    } | ConvertTo-Json -Compress
+
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($body)
+
+    $tmpFile = New-TemporaryFile
+    [System.IO.File]::WriteAllBytes($tmpFile, $bytes)
+
+    Write-Host "`n" 
+	
+	curl.exe -s -N `
+      -H "Accept: text/event-stream; charset=utf-8" `
+      -H "Content-Type: application/json; charset=utf-8" `
+      -X POST `
+      https://claude-chatbot-poc.onrender.com/chat/stream `
+      --data-binary "@$tmpFile" |
+
+	ForEach-Object {
+
+        if ($_ -match '^data:(.*)$') {
+
+            $content = $matches[1].Trim()
+
+            Write-Host $content
+        }
+    }
+
+    Remove-Item $tmpFile
+
+    Write-Host ""
+}
